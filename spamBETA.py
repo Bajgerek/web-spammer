83995237d4e0579671fc7e136aa8cf09e3f7944f
Zordon <65111609+Bajgerek@users.noreply.github.com>
2022-04-09 15:56:08 +0200

    1.0 fix

--- spamBETA.py
+++ spamBETA.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-driver = webdriver.Chrome(executable_path= r'D:\\webspammer\\chromedriver.exe')
+driver = webdriver.Chrome(executable_path= r'.\chromedriver.exe')
 
 
 while True:
